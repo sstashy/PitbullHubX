e9a30eb1c3fa9133472803aedd2d25b94dd6782d
G-MX2 <127357342+SoyAdriYT@users.noreply.github.com>
2024-10-31 23:52:25 -0600

    Update PitbullHubX.lua

--- PitbullHubX.lua
+++ PitbullHubX.lua
@@ -1,26 +1,17 @@
 --[[
-⣠⣶⠟⠛⠛⠛⠛⠛⣛⣻⣿⣿⣿⣿⣿⣟⣛⣛⣛⠛⠒⠲⠶⠦⣤⣤⣤⣀     
-         ⢀⣼⠏⠁  ⢀⣤⠶⣛⣩⣥⠤⠤⠤⠤⢤⣤⣤⣭⣭⣉⣉⣛⣛⣻⣭⣥⠬⡍⠛⢶⣄⡀       
-        ⢠⣾⠃  ⣠⡶⢋⡵⢛⡩⠵⠒⠒⠒⠒⠢⡀     ⢀⣠⠤⠤⠤⢤⣄   ⠉⠻⣆      
-       ⢀⣿⠃  ⠘⢁⡴⢋⣴⢿⠒⠈⠉⣏⠉⠐⠒⡾⣄     ⡠  ⢀⣀⣈⣙⣆⡀  ⢹⡆     
-      ⣠⣾⠃     ⢀⠟⣁ ⠁⢀⣤⣦⣤⡀⠘ ⢈⣷⡄   ⣇⠖⠉⠙⠅  ⠉⠉⠑⢦⡈⣷⡀    
-    ⢠⣾⢿⣧⠤⠤⠤⠄ ⠖⣿ ⠃  ⣿⣿⣿⣿⡗ ⠐⠁⢸⡇ ⣀⣰⠉⠠  ⣰⣶⣷⣶   ⢱⡈⢻⣦   
-   ⣠⡿⣱⠋⢀⣴⠶⠚⠻⢶⣤⡘⢧⣄⠆⠂ ⡉⠉⣉⣀⣀⠉⣠⡟⠁ ⠉⢻⣆   ⠘⠛⠟⠛  ⢈⡿⢍⢢⢹⡇  
-  ⢠⣿⠁⡇⢠⣿⠁ ⢰⣦⡀⠉⠉ ⠈⠙⠲⠾⠾⠶⠶⠶⠚⠋⠉    ⢸⣯⡑⠢⢤⣀⣂⣀⣨⠤⠒⠛⠃⠘⡆⡇⡧  
-  ⢸⣿ ⡇⢸⡇⢠⣴⣾⠋⠛⢷⣦⣀   ⠠⠤⠤⠴⢠⠶⠒      ⠉⢿⣦⡀    ⢸⣷  ⡼⢡⢣⡇  
-   ⢿⡇⣧⠘⠿  ⠸⣧⡀ ⠈⢻⡿⢶⣦⣄⡀  ⠸⣆⠐⠟⠻⠷   ⢀⣾⠛⠃⠑⠤ ⢀⣼⣿⡇⢀⠤⢂⣾⠃  
-   ⠈⢻⣌⠑⠦   ⢿⣿⣷⣤⣸⣷⡀ ⠈⠙⠻⢿⣶⣤⣄⣀⡀  ⠙⠿⠟⠁  ⢀⣠⡴⣿⠉⣿⣿  ⣼⠁   
-     ⠙⣷⡀   ⢸⣿⣿⣿⣿⣿⣿⣶⣤⣀⣀⣼⠁ ⠈⠉⠙⣿⠛⠛⠻⢿⠿⠛⠛⢻⡇ ⢸⡀⣹⣿  ⡏    
-      ⠈⢿⡀  ⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣤⣤⣄⣀⣿⣄⣀⣀⣸⣄⣀⣠⣴⣿⣶⣿⣿⣿⣿⡇ ⡇    
-       ⠈⢷⡄  ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇ ⡇    
-        ⠈⢿⣦ ⠘⣿⠛⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇ ⣷    
-          ⠙⢷⣄⠘⢷⡀⠘⡟⠿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇ ⣿    
-            ⠹⣧⡀⠻⣾⡃  ⠈⠙⢿⡿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇ ⣿    
-             ⠘⢿⣄⠈⠻⣦⡀  ⡼  ⠈⠙⠻⣿⠿⠿⠿⢿⣿⣿⣿⣿⣿⣿⣿⢿⡿⣹⠇ ⣿    
-               ⠹⣷⣄⠈⠛⠷⣼⣇⡀    ⣿   ⢸⡇  ⡿ ⢸⠇⣘⣧⠟ ⢀⡿    
-                ⠈⠛⢷⣄⡀ ⠙⠻⠷⠶⣶⣾⣿⣤⣀⣠⣿⣄⣀⣴⠷⠶⠿⠿⠟⠋ ⢀⣾⠃    
-                   ⠉⠛⠿⣶⣤⣤⣀⣀⡀        ⢀⣀⣀⣀⣀⣤⡤⠞⠁     
-                       ⠈⠉⠙⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠋⠉
+Dear Blade Ball players,
+
+We are pleased to announce that Pitbull Hub X has become the best script for the game Blade Ball. This script not only enhances the gaming experience but is also designed with a special purpose: to support a foundation dedicated to the welfare of dogs and cats.
+
+The key system implemented in Pitbull Hub X is a crucial tool for supporting this noble cause. Every key you acquire will directly contribute to our foundation, allowing us to provide care and shelter for animals in need.
+
+We kindly ask that you do not use methods to bypass the advertisements. These ads are essential for funding our operations and helping our four-legged friends. By supporting Pitbull Hub X, you not only gain an excellent script but also help make the world a better place for animals.
+
+Thank you for your understanding and support. Together, we can make a significant difference.
+
+Sincerely,
+
+
 • This file has been protected using Luraph
 • Script credits to G - MX2
 ]]--
